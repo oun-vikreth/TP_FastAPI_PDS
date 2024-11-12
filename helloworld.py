98eddbcdc0d76641f78f8e823730c329e8ad2687
oun-vikreth <96528416+oun-vikreth@users.noreply.github.com>
2024-11-12 08:32:52 +0700

    add cors & html

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- Change "*" to specific domains like ["http://127.0.0.1:5500"] for better security
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def hello_name(name: str):
